1cdfa9178ed76ce9dabeae3faa2c5b7e3d10f190
Marcus <marcusicaromc@gmail.com>
2022-12-09 01:04:16 -0300

    Create person table

---
+++ src/main/java/dev/marcusi/h2person/H2PersonApplication.java
@@ -0,0 +1,13 @@
+package dev.marcusi.h2person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2PersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2PersonApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table person (
+    id int auto_increment,
+    name varchar(255) NOT NULL,
+    surname varchar(255) NOT NULL,
+    weight double NOT NULL,
+    height double NOT NULL
+);
